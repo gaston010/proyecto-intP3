117ce422d11df5f8934079c10a5ee11ba0e900f5
Gaston <gaston010.gvmail.com>
2024-07-26 12:00:32 -0300

    add image

--- src/components/Underconstruction.jsx
+++ src/components/Underconstruction.jsx
@@ -1,8 +1,8 @@
+// import React from "react";
 import constructionImage from "../assets/underweb.jpeg";
 import constructionImage2 from "../assets/underweb2.jpeg";
 import constructionImage3 from "../assets/underweb3.jpeg";
 
-// Ajusta la ruta según sea necesario
 import { Link } from "react-router-dom";
 
 const images = [constructionImage, constructionImage2, constructionImage3];
@@ -12,17 +12,13 @@ function getRandomImage() {
   const randomIndex = Math.floor(Math.random() * images.length);
   return images[randomIndex];
 }
-function Underconstruction() {
 
+function Underconstruction() {
   const image = getRandomImage();
 
   return (
     <div style={styles.container}>
-      <img
-        src={constructionImage}
-        alt="Under Construction"
-        style={styles.image}
-      />
+      <img src={image} alt="Under Construction" style={styles.image} />
       <div style={styles.textContainer}>
         <h1 style={styles.text}>We are currently under construction!</h1>
         <Link to="/" style={styles.link}>
@@ -50,14 +46,22 @@ const styles = {
     left: 0,
     width: "100%",
     height: "100%",
-    objectFit: "cover", // Esto hace que la imagen cubra el área sin distorsionarse
+    objectFit: "cover", 
     zIndex: -1, // Asegura que la imagen esté detrás del texto
   },
-  text: {
+  textContainer: {
     position: "relative",
     zIndex: 1, // Asegura que el texto esté por encima de la imagen
-    color: "#F32", // Ajusta el color del texto según sea necesario
+  },
+  text: {
+    color: "#F32", 
     fontSize: "24px",
+  },
+  link: {
+    textDecoration: "none", 
+  },
+  button: {
+    marginTop: "20px",
   },
 };
 
