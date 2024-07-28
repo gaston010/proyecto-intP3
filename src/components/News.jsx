3d7ab17a4bde897a7f2bbb0660cfa73ee04ed6d2
Ger678 <germanalejandroaguirre678@gmail.com>
2024-07-28 11:14:14 -0300

    2024-07-28 | News section with carousel component

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import Cookies from 'js-cookie';
-// import { Carousel } from '@react-spring/web';
-import { useSpringCarousel } from 'react-spring-carousel'
-import { element } from 'prop-types';
+import Carousel from './Carousel';
 
-function News() {
+export function News() {
   const [newsData, setNewsData] = useState([]);
 
   useEffect(() => {
@@ -18,7 +16,7 @@ function News() {
           }
         });
         if (response.ok) {
-          const data = await response.json();
+          var data = await response.json();
           data.forEach((item) => {
             if (item.image === null) {
               item.image = 'src/assets/underweb.jpeg';
@@ -30,56 +28,21 @@ function News() {
         console.error('Error fetching artist news data:', error);
       }
     };
-    array.forEach(element => {
-      
-    });
-    fetchData().then((d) => {
-      for (let index = 0; index < d.length; index++) {
-        const obj = d[index]; ;
-        element =
-            {
-              id: obj.id,
-              renderItem: (
-                <div key={obj.id} className="news-card">
-                <div className='img'>
-                  <img src={obj.image} alt={obj.name} />
-                </div>
-                <div className="card-content">
-                  <h2>{obj.name}</h2>
-                  <p>{obj.bio}</p>
-                  <a href={obj.website}>Learn More</a>
-                </div>
-              </div>
-              )
-            };
-        
-          let carouselObj = useSpringCarousel(element);
-          newsData.push(carouselObj);
-          debugger;
-      }
-    });
+
+    fetchData();
+    
   }, []);
 
 
   return (
-    <div className='grid-container'>
+    <div className="flex">
       <h1>News</h1>
       <p>Welcome to the news page!</p>
-
       
-      <div className="news-grid">
-        {newsData.map((news) => (
-          <div key={news.id} className="news-card">
-            <div className='img'>
-              <img src={news.image} alt={news.name} />
-            </div>
-            <div className="card-content">
-              <h2>{news.name}</h2>
-              <p>{news.bio}</p>
-              <a href={news.website}>Learn More</a>
-            </div>
-          </div>
-        ))}
+      <div className='flex'>
+        <div className="grid justify-center">
+          <Carousel data={newsData}></Carousel>
+        </div>
       </div>
     </div>
   );
