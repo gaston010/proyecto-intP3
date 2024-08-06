aa397b4aff13b55aa0fa84d793fe6ae991dc6d36
Gaston <gaston010.gv@gmail.com>
2024-08-06 17:51:32 -0300

    ```text feat: Add MainLayout component for managing layout structure
    Add the MainLayout component to handle the overall layout structure of the application. This component includes the Navbar, SideMenu, Background, PlaybackBar, and Outlet components, and manages the state for the side menu, token, and background visibility. It also provides the MediaProvider context for managing media-related data. The MainLayout component is responsible for rendering the appropriate components based on the current location and authentication token.

---
+++ src/components/MainLayout/MainLayout.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import Background from "../Background.jsx";
+import PlaybackBar from "../PlaybackBar/PlaybackBar.jsx";
+import SideMenu from "../SideMenu";
+import Navbar from "../Navbar";
+import { Outlet, useLocation } from "react-router-dom";
+import Cookies from "js-cookie";
+import { MediaProvider } from "../../context/MediaContext";
+
+const MainLayout = () => {
+  const location = useLocation();
+  const [isSideMenuOpen, setIsSideMenuOpen] = useState(true);
+  const token = Cookies.get("authToken");
+
+  const noBackgroundRoutes = ["/underconstruction"];
+  const noNavBarRoutes = ["/", "/login"];
+
+  const showBackground = !noBackgroundRoutes.includes(location.pathname);
+  const homePath = noNavBarRoutes.includes(location.pathname);
+
+  const toggleSideMenu = () => {
+    setIsSideMenuOpen(!isSideMenuOpen);
+  };
+
+  return (
+    <MediaProvider>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          minHeight: "100vh",
+          minWidth: "100vw",
+        }}
+      >
+        <div style={{ flex: "1" }}>
+          {homePath || !token ? (
+            <Navbar />
+          ) : (
+            <SideMenu
+              className={isSideMenuOpen ? "open" : "closed"}
+              toggleSideMenu={toggleSideMenu}
+              style={{ flex: "1", minHeight: "100vh" }}
+            />
+          )}
+        </div>
+        <section
+          className={isSideMenuOpen ? "with-sidemenu" : "without-sidemenu"}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            flex: 1,
+          }}
+        >
+          <main className="main" style={{ flex: 1 }}>
+            {showBackground ? (
+              <Background>
+                <Outlet />
+              </Background>
+            ) : (
+              <Outlet />
+            )}
+          </main>
+          {token && <PlaybackBar />}
+        </section>
+      </div>
+    </MediaProvider>
+  );
+};
+
+export default MainLayout;
