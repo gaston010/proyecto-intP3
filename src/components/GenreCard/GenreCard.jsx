478edcda4839ac52166b7abf507ba04fcab25c41
Gaston <gaston010.gv@gmail.com>
2024-08-06 17:49:01 -0300

    feat: Add GenreCard component
    Add the GenreCard component along with its associated CSS file to display genre information and handle user interactions.

---
+++ src/components/GenreCard/GenreCard.jsx
@@ -0,0 +1,34 @@
+import PropTypes from "prop-types";
+import "bulma/css/bulma.min.css";
+import "./GenreCard.css"; // Import the CSS file
+
+const GenreCard = ({ genre, onClick, onEdit }) => {
+  return (
+    <div className="column is-half">
+      <div className="card" onClick={onClick}>
+        <div className="card-content card-content-genre">
+          <p className="title is-4 has-text-centered">{genre.name}</p>
+          <p className="subtitle is-5 has-text-centered">{genre.description}</p>
+          <button className="button is-link is-fullwidth">View Details</button>
+          <button
+            className="button is-info is-fullwidth"
+            onClick={(e) => {
+              e.stopPropagation(); // Prevent the click event from bubbling up
+              onEdit(genre);
+            }}
+          >
+            Edit
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+GenreCard.propTypes = {
+  genre: PropTypes.object.isRequired,
+  onClick: PropTypes.func.isRequired,
+  onEdit: PropTypes.func.isRequired,
+};
+
+export default GenreCard;
