ad7e9eb0894bf249bbe86c2392a21ec17967ec3e
Gaston <gaston010.gv@gmail.com>
2024-08-06 17:52:32 -0300

    feat: Add PrivateRoute component for handling private routes
    Add the PrivateRoute component to handle private routes in the application. This component checks for the presence of an authentication token and redirects the user to the login page if the token is not found. It ensures that only authenticated users can access certain routes. The PrivateRoute component improves the security and user experience of the application.

---
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -0,0 +1,19 @@
+import { Navigate } from "react-router-dom";
+import Cookies from "js-cookie";
+import PropTypes from "prop-types";
+
+const PrivateRoute = ({ children }) => {
+  const token = Cookies.get("authToken");
+
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+};
+
+PrivateRoute.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default PrivateRoute;
