f7b4762e4eba954df845631f57f1ac29def5fc85
Gaston <gaston010.gv@gmail.com>
2024-08-06 17:55:38 -0300

    refactor: Update MediaProvider component to use double quotes consistently for import statements and prop types

--- src/context/MediaContext.jsx
+++ src/context/MediaContext.jsx
@@ -1,15 +1,29 @@
-import React, { createContext, useState } from 'react';
+import { createContext, useState } from "react";
+import PropTypes from "prop-types";
 
 export const MediaContext = createContext();
 
 export const MediaProvider = ({ children }) => {
   const [mediaFile, setMediaFile] = useState(null);
-  const [title, setTitle] = useState('');
+  const [title, setTitle] = useState("");
   const [duration, setDuration] = useState(0);
 
   return (
-    <MediaContext.Provider value={{ mediaFile, setMediaFile , title, setTitle, duration, setDuration}}>
+    <MediaContext.Provider
+      value={{
+        mediaFile,
+        setMediaFile,
+        title,
+        setTitle,
+        duration,
+        setDuration,
+      }}
+    >
       {children}
     </MediaContext.Provider>
   );
+};
+
+MediaProvider.propTypes = {
+  children: PropTypes.node.isRequired,
 };
