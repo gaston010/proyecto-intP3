2be484ecae6a12aa5e4ab0c4673adb866f2d4ab0
rmarting13 <rmarting13@gmail.com>
2024-08-08 20:48:21 -0300

    SongList integration with custom hook useSongsList

--- src/components/hooks/useSongsList.js
+++ src/components/hooks/useSongsList.js
@@ -6,14 +6,24 @@ const useSongsList = ({url}) => {
     const [loading, setLoading] = useState(false);
     const [next, setNext] = useState(null);
     const [previous, setPrevious] = useState(null);
+    const [current, setCurrent] = useState(null);
 
     const [result] = useFetch(url);
 
     useEffect(() => {
-        setLoading(result.isLoading);
-        setNext(result.data.next);
-        setPrevious(result.data.previous);
+        if(!result.isError){
+            setLoading(result.isLoading);
+            setNext(result.data.next);
+            setPrevious(result.data.previous);
+            setCurrent(result.data);
+        } else {
+            setLoading(false);
+            setNext(null);
+            setPrevious(null);
+            setCurrent(null);
+        }
+        
     }, [result]);
 
-    return [loading, next, previous, result];
+    return [loading, previous, next, result];
 }
--- src/components/playlist/SongsList.jsx
+++ src/components/playlist/SongsList.jsx
@@ -1,1 +1,72 @@
-import {useSongsList} from "../hooks/useSongsList";
+import { useSongsList } from "../hooks/useSongsList";
+
+import { useState, useEffect, useContext } from 'react';
+import PropTypes from 'prop-types';
+
+import { MediaContext } from '../context/MediaContext';
+
+// const SongList = ({ songIds }) => {
+const SongList = () => {
+    const [songs, setSongs] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [next, setNext] = useState(null);
+    const [previous, setPrevious] = useState(null);
+
+    const [url, setUrl] = useState('');
+    const [currPage, pervPage, nextPage] = useSongsList(url);
+
+  useEffect(() => {
+    setUrl('http://sandbox.academiadevelopers.com/harmonyhub/songs/?page=1');
+  }, []);
+
+  const fetchSongs = async (url) => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setSongs(data.results);
+      setNext(data.next);
+      setPrevious(data.previous);
+    } catch (error) {
+      setError(error);
+      console.error('Error fetching data:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  if (loading) {
+    return <p>Loading songs...</p>;
+  }
+
+  if (error) {
+    return <p>Error loading songs: {error}</p>;
+  }
+
+  const handleNext = () => {
+    fetchSongs(next);
+  }
+  const handlePrevious = () => {
+    if (!previous) {
+      fetchSongs('http://sandbox.academiadevelopers.com/harmonyhub/songs/?page=1');
+    } else {
+      fetchSongs(previous);
+    }
+  }
+
+  return (
+    <div>
+        <div className="song-action-buttons">
+          <button onClick={handlePrevious}>⏮️</button>
+          <button onClick={handleNext}>⏭️</button>
+        </div>
+      <div className="song-grid">
+        
+        {songs.map((song) => (
+          <SongCard key={song.id} song={song} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
