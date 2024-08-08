0041f72632396daf9cd1a865a1ec2729b6405fb4
Gaston <gaston010.gv@gmail.com>
2024-08-08 15:03:53 -0300

    feat: Add AddArtistAssignment component for assigning artists add the /add for test mode

--- src/Router.jsx
+++ src/Router.jsx
@@ -7,6 +7,7 @@ import MainLayout from "./components/MainLayout";
 import Underconstruction from "./components/Underconstruction";
 import GenreList from "./components/GenreList";
 import SongList from "./components/SongList";
+import AddArtistAssignment from "./components/AddArtistAssignment";
 // import Sidebar from "./components/Sidebar";
 import PrivateRoute from "./components/PrivateRoute"; // Asegúrate de importar PrivateRoute
 
@@ -46,7 +47,11 @@ const Router = createBrowserRouter([
       {
         path: "/songs",
         element: <SongList />,
-      }
+      },
+      {
+        path: "/add",
+        element: <AddArtistAssignment />,
+      },
     ],
   },
   {
