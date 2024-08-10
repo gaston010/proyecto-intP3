f750f6358f363cf443350c26e7c97ff6c59bbe23
rmarting13 <rmarting13@gmail.com>
2024-08-10 00:33:12 -0300

    Custom Hook useSong integrated within component MediaPlayer

---
+++ src/components/playlist/MediaPlayer.jsx
@@ -0,0 +1,150 @@
+// PlaybackBar.jsx
+import React, { useState, useContext, useEffect, useRef } from 'react';
+import { MediaContext } from '../context/MediaContext';
+import Cookies from 'js-cookie';
+import useSong from '../hooks/useSong';
+
+const MediaPlayer = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [volume, setVolume] = useState(50);
+  const [progress, setProgress] = useState(0);
+  const { mediaFile, title, duration, prev, next } = useContext(MediaContext);
+  const audioRef = useRef(null);
+
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.volume = volume / 100;
+    }
+  }, [volume]);
+
+  useEffect(() => {
+    if (audioRef.current) {
+      if (isPlaying) {
+        audioRef.current.play();
+      } else {
+        audioRef.current.pause();
+      }
+    }
+  }, [isPlaying]);
+
+  useEffect(() => {
+    if (mediaFile && audioRef.current) {
+      audioRef.current.src = mediaFile;
+      audioRef.current.load();
+      if (!isPlaying) {
+        togglePlayPause();
+        audioRef.current.play();
+      }
+    }
+  }, [mediaFile]);
+
+  useEffect(() => {
+    const audio = audioRef.current;
+    if (audio) {
+      const updateProgress = () => {
+        setProgress((audio.currentTime / audio.duration) * 100);
+      };
+      audio.addEventListener('timeupdate', updateProgress);
+      return () => {
+        audio.removeEventListener('timeupdate', updateProgress);
+      };
+    }
+  }, []);
+
+  const togglePlayPause = () => {
+    setIsPlaying(!isPlaying);
+  };
+
+  const handleVolumeChange = (e) => {
+    setVolume(e.target.value);
+  };
+
+  const handleProgressChange = (e) => {
+    setProgress(e.target.value);
+    if (audioRef.current) {
+      audioRef.current.currentTime =
+        (audioRef.current.duration * e.target.value) / 100;
+    }
+  };
+
+  const handleNext = (e) => {
+    useSong(prev);
+  };
+
+  const handlePrev = (e) => {
+    useSong(next);
+  }
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+  };
+
+  const token = Cookies.get("authToken");
+
+  return (
+    <div
+      className="bottom-0 bg-gray-900 text-white flex items-center justify-between p-4"
+      style={{
+        display: `${token ? 'flex':'none'}`,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        bottom: '0',
+        background: 'hsl(220deg 13.04% 9.02%)',
+        boxShadow: '0px -1px 5px 0px rgba(0,0,0,0.75)',
+        minWidth: '-webkit-fill-available'
+      }}
+    >
+      <h1>{title}</h1>
+      <audio ref={audioRef} />
+      <div
+        className="flex flex-direction-column justify-items-center"
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center'
+        }}
+      >
+        <div className="flex items-center space-x-4">
+          <button onClick={() => console.log('Previous')}>⏮️</button>
+          <button onClick={togglePlayPause} style={{ fontSize: '2rem' }}>
+            {isPlaying ? '⏸️' : '▶️'}
+          </button>
+          <button onClick={handleNext}>⏭️</button>
+        </div>
+        <div className="flex-1 mx-4">
+          <div className="flex justify-between">
+            <span>
+              {formatTime(
+                (audioRef.current && audioRef.current.currentTime) || 0
+              )}--  
+            </span>
+            <input
+              type="range"
+              min="0"
+              max={duration}
+              value={progress}
+              onChange={handleProgressChange}
+              style={{ width: '30rem' }}
+            />
+            <span>--  {formatTime(duration)}</span>
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center space-x-2">
+        <span>🔊</span>
+        <input
+          type="range"
+          min="0"
+          max="100"
+          value={volume}
+          onChange={handleVolumeChange}
+          className="w-24"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default MediaPlayer;
