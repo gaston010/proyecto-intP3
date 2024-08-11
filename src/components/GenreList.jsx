00dc82d4a250a80260ceb3262d1900e584bf2634
Gaston <gaston010.gv@gmai.com>
2024-08-11 19:02:06 -0300

    chore: Update GenreList component to fetch genres with pagination and add loading state

--- src/components/GenreList.jsx
+++ src/components/GenreList.jsx
@@ -11,25 +11,31 @@ const GenreList = () => {
   const [selectedGenreId, setSelectedGenreId] = useState(null);
   const [showForm, setShowForm] = useState(false);
   const [editingGenre, setEditingGenre] = useState(null);
+  const [nextPageUrl, setNextPageUrl] = useState(null);
+  const [previousPageUrl, setPreviousPageUrl] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchGenres = async (url) => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      if (data && Array.isArray(data.results)) {
+        setGenres(data.results);
+        setNextPageUrl(data.next);
+        setPreviousPageUrl(data.previous);
+      } else {
+        console.error("Expected an array of genres in results");
+      }
+    } catch (error) {
+      console.error("Error fetching genres:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const fetchGenres = async () => {
-      try {
-        const response = await fetch(
-          "https://sandbox.academiadevelopers.com/harmonyhub/genres/"
-        );
-        const data = await response.json();
-        if (data && Array.isArray(data.results)) {
-          setGenres(data.results);
-        } else {
-          console.error("Expected an array of genres in results");
-        }
-      } catch (error) {
-        console.error("Error fetching genres:", error);
-      }
-    };
-
-    fetchGenres();
+    fetchGenres("https://sandbox.academiadevelopers.com/harmonyhub/genres/");
   }, []);
 
   const handleGenreClick = (id) => {
@@ -51,46 +57,68 @@ const GenreList = () => {
         genre.id === updatedGenre.id ? updatedGenre : genre
       )
     );
-    setEditingGenre(null); // Reset the editing genre state
+    setEditingGenre(null);
   };
 
   return (
     <div className="flex flex-col items-center">
       <h1 className="text-2xl font-bold mb-4">Music Genres</h1>
-      <div className="flex flex-wrap w-full">
-        <div className="w-full lg:w-2/3 p-4">
-          <div className="flex flex-wrap">
-            {genres.map((genre) => (
-              <GenreCard
-                key={genre.id}
-                genre={genre}
-                onClick={handleGenreClick}
-                onEdit={handleEditGenre}
+      {loading ? (
+        <p>Loading Genres..</p>
+      ) : (
+        <div className="flex flex-wrap w-full">
+          <div className="w-full lg:w-2/3 p-4">
+            <div className="flex flex-wrap">
+              {genres.map((genre) => (
+                <GenreCard
+                  key={genre.id}
+                  genre={genre}
+                  onClick={handleGenreClick}
+                  onEdit={handleEditGenre}
+                />
+              ))}
+            </div>
+            <div className="flex justify-between mt-4">
+              {previousPageUrl && (
+                <button
+                  className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700"
+                  onClick={() => fetchGenres(previousPageUrl)}
+                >
+                  Previous
+                </button>
+              )}
+              {nextPageUrl && (
+                <button
+                  className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700"
+                  onClick={() => fetchGenres(nextPageUrl)}
+                >
+                  Next
+                </button>
+              )}
+            </div>
+          </div>
+          <div className="w-full lg:w-1/3 p-4">
+            <GenreDetail genreId={selectedGenreId} />
+            <button
+              className="mt-4 w-full bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700"
+              onClick={() => setShowForm(!showForm)}
+            >
+              <span className="icon">
+                <i className="fas fa-plus"></i>
+              </span>
+              <span>Add New Genre</span>
+            </button>
+            {showForm && <AddGenreForm addGenreToList={addGenreToList} />}
+            {editingGenre && (
+              <UpdateGenreForm
+                genre={editingGenre}
+                onGenreUpdated={handleGenreUpdated}
               />
-            ))}
+            )}
+            {selectedGenreId && <SongList genreId={selectedGenreId} />}
           </div>
         </div>
-        <div className="w-full lg:w-1/3 p-4">
-          <GenreDetail genreId={selectedGenreId} />
-          <button
-            className="mt-4 w-full bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700"
-            onClick={() => setShowForm(!showForm)}
-          >
-            <span className="icon">
-              <i className="fas fa-plus"></i>
-            </span>
-            <span>Add New Genre</span>
-          </button>
-          {showForm && <AddGenreForm addGenreToList={addGenreToList} />}
-          {editingGenre && (
-            <UpdateGenreForm
-              genre={editingGenre}
-              onGenreUpdated={handleGenreUpdated}
-            />
-          )}
-          {selectedGenreId && <SongList genreId={selectedGenreId} />}
-        </div>
-      </div>
+      )}
     </div>
   );
 };
@@ -122,9 +150,9 @@ const SongList = ({ genreId }) => {
         {songs.map((song) => (
           <div key={song.id} className="bg-gray-700 text-white p-2 rounded">
             <h3 className="text-md">{song.title}</h3>
-            <p className="text-sm">Año: {song.year}</p>
-            <p className="text-sm">Duración: {song.duration} segundos</p>
-            <p className="text-sm">Vistas: {song.view_count}</p>
+            <p className="text-sm">Year: {song.year}</p>
+            <p className="text-sm">Duration: {song.duration} segundos</p>
+            <p className="text-sm">Played: {song.view_count}</p>
           </div>
         ))}
       </div>
